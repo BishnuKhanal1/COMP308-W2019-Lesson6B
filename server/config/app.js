d04956245d669acaa1457ee405cc2291378e5e5b
Mr. Bishnu Prasad Khanal <bkhanal1@my.centennialcollege.ca>
2019-02-16 14:40:06 -0500

    Created User Model

--- server/config/app.js
+++ server/config/app.js
@@ -1,41 +1,73 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+//modules for the node and express
+let createError = require("http-errors");
+let express = require("express");
+let path = require("path");
+let cookieParser = require("cookie-parser");
+let logger = require("morgan");
+
+//modules for authentications
+
+let session = require("express-session");
+let passport = require("passport");
+let passportlocal = require("passport-local");
+let localStrategy = passportlocal.Strategy;
+let flash = require("connect-flash");
 
 // database setup
-let mongoose = require('mongoose');
-let DB = require('./db');
+let mongoose = require("mongoose");
+let DB = require("./db");
 
 // point Mongoose to the DB URI
 mongoose.connect(DB.URI);
 
 let mongoDB = mongoose.connection;
-mongoDB.on('error', console.error.bind(console, 'Connection Error:'));
-mongoDB.once('open', ()=> {
+mongoDB.on("error", console.error.bind(console, "Connection Error:"));
+mongoDB.once("open", () => {
   console.log("Connected to MongoDB...");
 });
 
-let indexRouter = require('../routes/index');
-let contactRouter = require('../routes/contact');
-
+let indexRouter = require("../routes/index");
+let contactRouter = require("../routes/contact");
 
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../../public')));
-app.use(express.static(path.join(__dirname, '../../node_modules')));
+app.use(express.static(path.join(__dirname, "../../public")));
+app.use(express.static(path.join(__dirname, "../../node_modules")));
 
-app.use('/', indexRouter);
-app.use('/contact-list', contactRouter);
+// setup express-session
+app.use(
+  session({
+    secret: "SomeSecret",
+    saveUninitialized: false,
+    resave: false
+  })
+);
+
+// initialize flash
+app.use(flash());
+
+//initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//passport user configuration
+
+//create a user model
+
+//implemement a user authentication strategy
+
+//seralize and deserialize the user info
+
+app.use("/", indexRouter);
+app.use("/contact-list", contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -46,11 +78,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
